06f0c1b932744df0a60802a282e14e1b826d08ca
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-11 19:42:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ while op_cliente != "0":
     print()
     if op_cliente == "1":
         clientes.cadastrar_cliente()
+    elif op_cliente == "2":
+        clientes.exibit_cliente()
+    elif op_cliente == "5":
+        clientes.relatorio_clientes()
