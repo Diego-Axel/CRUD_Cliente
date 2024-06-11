6e7f14b63243414a588f16453f09ce05f1a5e959
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-11 19:43:05 -0300

    Update Sel.py

--- Sel.py
+++ Sel.py
@@ -1,3 +1,8 @@
 def select_query():
     select_query = "SELECT * FROM clientes;"
     return select_query
+
+
+def where_query():
+    select_where_query = "SELECT * FROM clientes WHERE cod_cliente = %s"
+    return select_where_query
