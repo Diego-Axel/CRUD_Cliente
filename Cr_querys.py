a1ee79f3bda64acf0aa75c9901047bc98bb711c8
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-11 18:22:29 -0300

    Create Cr_querys.py

---
+++ Cr_querys.py
@@ -0,0 +1,12 @@
+def create_table():
+    create_table_query = '''
+        CREATE TABLE IF NOT EXISTS clientes(
+        cod_cliente SERIAL PRIMARY KEY,
+        nome VARCHAR(60),
+        email VARCHAR(40),
+        celular VARCHAR(25),
+        cpf VARCHAR(20),
+        ativo BOOLEAN
+    );
+    '''
+    return create_table_query
