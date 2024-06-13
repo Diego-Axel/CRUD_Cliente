876c241f5c258d5bbeec1096dc06df58d642d016
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-13 17:49:47 -0300

    Update Insr_query.py

--- Insr_query.py
+++ Insr_query.py
@@ -1,7 +1,6 @@
 '''
-Arquivo para as minhas querys referentes a inserção de valores nas tabelas, INSERT INTO, VALUES.
+Arquivo para as minhas querys referentes a inserção de valores nas tabelas, INSERT INTO.
 '''
-
 def insert_into():
     insert_query = '''
         INSERT INTO clientes (nome, email, celular, cpf, ativo)
