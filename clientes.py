69518dc034f83a6a12c5ef1ceec04ba8e482b03e
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-15 22:26:13 -0300

    Update clientes.py

--- clientes.py
+++ clientes.py
@@ -125,10 +125,89 @@ def exibir_cliente():
 
 
 def alterar_dados(): # Em desenvolvimento
-    pass
+    try:
+        connection = psycopg2.connect(
+            user="postgres",
+            password="palmeiras123",
+            host="localhost",
+            port="5432",
+            database="clientes"
+        )
+        cursor = connection.cursor()
+
+        # Pedindo os Dados do Cliente a ser Alterado
+        os.system('clear || cls')
+        print()
+        print("############################################")
+        print("#####        Alterar Cliente            #####")
+        print("############################################")
+        print()
+        cod_cliente = input("##### Digite o ID do cliente a ser alterado: ")
+        print()
+        nome_cliente = input("##### Nome: ")
+        print()
+        email = input("#### E-mail: ")
+        print()
+        celular = input("##### Digite seu Celular: ")
+        print()
+        cpf = input("##### CPF: ")
+
+        # Atualizando Dados na Tabela:
+        update_query = """
+        UPDATE clientes 
+        SET nome = %s, email = %s, celular = %s, cpf = %s 
+        WHERE id = %s;
+        """
+        cursor.execute(update_query, (nome_cliente, email, celular, cpf, cod_cliente))
+        connection.commit()
+        print(f"Cliente com ID {cod_cliente} atualizado com sucesso")
+        print()
+        input("tecle <ENTER> para prosseguir ")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Erro ao conectar ou operar no PostgrSQL", error)
+    finally:
+        # Fechar Conexão
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Conexão com PostgrSQL fechada")
 
 def excluir_clienre(): # Em desenvolvimento
-    pass
+    try:
+        connection = psycopg2.connect(
+            user="postgres",
+            password="palmeiras123",
+            host="localhost",
+            port="5432",
+            database="clientes"
+        )
+        cursor = connection.cursor()
+
+        # Pedindo o ID do cliente a ser excluído
+        os.system('clear || cls')
+        print()
+        print("############################################")
+        print("#####        Excluir Cliente            #####")
+        print("############################################")
+        print()
+        cod_cliente = input("##### Digite o ID do cliente a ser excluído: ")
+
+        # Excluindo Dados na Tabela:
+        delete_query = f"DELETE FROM clientes WHERE id = {cod_cliente};"
+        cursor.execute(delete_query)
+        connection.commit()
+        print(f"Cliente com ID {cod_cliente} excluído com sucesso")
+        print()
+        input("tecle <ENTER> para prosseguir ")
+
+
+    finally:
+        # Fechar Conexão
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Conexão com PostgreSQL fechada")
 
 
 def relatorio_clientes():
