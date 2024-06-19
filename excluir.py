50d6749fc907a4dd88ae71a911b17392afb06f3b
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-19 18:23:20 -0300

    Update excluir.py

--- excluir.py
+++ excluir.py
@@ -9,7 +9,6 @@ import os
 def excluir_cliente():
     connection = None
     cursor = None
-    
     try:
         connection = psycopg2.connect(
             user="postgres",
@@ -19,26 +18,19 @@ def excluir_cliente():
             database="clientes"
         )
         cursor = connection.cursor()
-
-        # Pedindo o ID do cliente a ser excluído
-        os.system('clear || cls')
+        os.system('clear || cls') # se for Linux use 'clear' e se for Windowns use 'cls'
         print()
         print("############################################")
         print("#####          Excluir Cliente         #####")
         print("############################################")
         print()
-        cod_cliente = input("##### Digite o ID do cliente a ser excluído: ")
-
-        # Definindo a query de exclusão
-        del_query.delete_query()
-
-        # Executando a query de exclusão
-        cursor.execute(del_query.delete_query(), (cod_cliente,))
+        cod_cliente = input("##### Digite o ID do cliente a ser excluído: ") # Pedindo o ID do cliente a ser excluído
+        del_query.delete_query() # Definindo a query de exclusão
+        cursor.execute(del_query.delete_query(), (cod_cliente,)) # Executando a query de exclusão
         connection.commit()
         print(f"Cliente com ID {cod_cliente} excluído com sucesso")
         print()
         input("tecle <ENTER> para prosseguir ")
-
     except (Exception, psycopg2.Error) as error:
         print("Erro ao conectar ou operar no PostgreSQL", error)
         input("tecle <ENTER> para prosseguir ")
