f126c7f4ab1e1cc42af92f4284352c57bcaf060c
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-18 23:30:51 -0300

    Update Del_query.py

--- Del_query.py
+++ Del_query.py
@@ -2,6 +2,6 @@
 Arquivo para as minhas querys referentes para deletar um cliente, DELETE FROM.
 '''
 
-def delete_query(cod_cliente):
-    delete_query = f"DELETE FROM clientes WHERE id = {cod_cliente};"
+def delete_query():
+    delete_query = "DELETE FROM clientes WHERE cod_cliente = %s"
     return delete_query
