995dc04092d78ab11841dca78c9542099df80487
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-18 23:30:55 -0300

    Update sel_query.py

--- sel_query.py
+++ sel_query.py
@@ -2,10 +2,15 @@
 Arquivo para as minhas querys referentes a SELECT(s).
 '''
 def select_query():
-    select_query = "SELECT * FROM clientes;"
+    select_query = "SELECT cod_cliente, nome, email, celular, cpf FROM clientes WHERE cod_cliente = %s"
     return select_query
 
 
+def select_full_query():
+    select_full_query = "SELECT * FROM clientes;"
+    return select_full_query
+
+
 def where_query():
-    select_where_query = "SELECT * FROM clientes WHERE cod_cliente = %s"
+    select_where_query = "SELECT * FROM clientes WHERE cod_cliente = %d"
     return select_where_query
