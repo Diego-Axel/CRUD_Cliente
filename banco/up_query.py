e227eb70c5886f6fb1577f478a7342e4ef43486b
Diego-Axel <diegoaxelbsr@gmail.com>
2024-07-16 18:00:45 -0300

    Update up_query.py

--- banco/up_query.py
+++ banco/up_query.py
@@ -3,11 +3,11 @@ Arquivo para as minhas querys referentes para Alterar os dados do Cliente, UPDAT
 '''
 
 def update_query():
-    update_query = """
+    update_query = '''
         UPDATE clientes
         SET nome = %s, email = %s, celular = %s, cpf = %s
         WHERE cod_cliente = %s
-        """
+        '''
     return update_query
 
 
